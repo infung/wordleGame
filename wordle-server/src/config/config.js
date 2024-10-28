628666d7cf9f95776259543be686be438bddc3f8
Yin Feng <y77feng@uwaterloo.ca>
2024-10-28 10:52:24 +0800

    update icon

--- wordle-server/src/config/config.js
+++ wordle-server/src/config/config.js
@@ -1,5 +1,8 @@
 // Configuration file for Wordle game settings
+
 module.exports = {
     maxRounds: 6, // Maximum number of rounds allowed
-    wordList: ['apple', 'grape', 'peach', 'berry', 'melon'], // List of possible answers
+    wordList: ['apple', 'grape', 'peach', 'berry', 'melon', 'brave', 'crisp', 
+              'drift', 'eagle', 'flame', 'globe', 'haste', 'ivory', 'jazzy', 
+              'noble', 'ocean', 'piano', 'pearl', 'scoop', 'honey', 'lucky'], // List of possible answers
   };
